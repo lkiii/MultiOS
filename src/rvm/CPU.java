4077d0f61aca0206ac3c93095bbb95389750bfba
lukasi <alach@bmx.lt>
2012-02-25 18:47:03 +0200

    CPU

--- src/rvm/CPU.java
+++ src/rvm/CPU.java
@@ -1,7 +1,28 @@
 package rvm;
 
 public class CPU {
+
     private Word PTR;
-    
-    
+    private Word R1;
+    private Word R2;
+    private byte[] IP;
+    private boolean C;
+    private byte MODE;
+    private Timer TIMER;
+    private byte TI;
+    private byte IOI;
+    private byte SI;
+    private byte PI;
+    private boolean CH1;
+    private boolean CH2;
+    private boolean CH3;
+
+    public CPU() {
+        IP = new byte[2];
+        TIMER = new Timer();
+        TI = 0;
+        IOI = 0;
+        SI = 0;
+        PI = 0;
+    }
 }
