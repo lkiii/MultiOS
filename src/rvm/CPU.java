543e4288c39e359aad97aef3c0efbe2e39905d64
ernestas <ernestas@192.168.1.6>
2012-02-25 17:55:19 +0200

    Externala ximichinu

--- src/rvm/CPU.java
+++ src/rvm/CPU.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rvm;
 
-/**
- *
- * @author ernestas
- */
 public class CPU {
     private Word PTR;
+    
+    
 }
