82be9bec9aca1661efea1f0e5aa53ac065796089
ernestas <ernestas@192.168.1.6>
2012-05-17 22:34:02 +0300

    Planuotojas. PQueue ir Comparatoriaus pagrindu

--- src/MOS/Planner.java
+++ src/MOS/Planner.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package MOS;
 
 import rvm.RM;
@@ -13,7 +9,7 @@ import rvm.RM;
  *   foreach'as ieskantis geriausio
  * @author ernestas
  */
-public class Planner {
+public class Planner { 
     private RM machine;
     
     /**
